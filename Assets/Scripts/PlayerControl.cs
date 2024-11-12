a9432245d53fb1399baf7468b51e18b84b764723
Rikatsuu <erirainn@gmail.com>
2024-11-11 20:57:56 -0800

    Test Build V1
    Implemented Ihiyo's Room and added temp assets, animations, and walking functionality

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+    private Rigidbody2D rb;
+    private Vector2 moveDir;
+    private Animator animator;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>(); //Accesses the RigidBody Component that are both attached to the same object
+        animator = GetComponent<Animator>(); //Access character animation
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //Reads User Input
+    }
+
+    void FixedUpdate() {
+
+        bool walker = moveDir.magnitude > 0;
+        animator.SetBool("isWalking", true); //Connects the Bool val
+
+        if(!walker) { //Checked if we stopped moving
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveDir.x);
+            animator.SetFloat("LastInputX", moveDir.y);
+        }
+
+        rb.velocity = moveDir.normalized * moveSpeed; //Physics Based Movement
+        animator.SetFloat("InputX", moveDir.x);  
+        animator.SetFloat("InputY", moveDir.y);
+    }
+}
