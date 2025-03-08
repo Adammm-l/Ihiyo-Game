5bf86388b56a0d6d7721c22384eb8599ca8ea2a6
BlKoolaid <theflyingmansion@gmail.com>
2025-03-08 13:32:23 -0800

    pushing all the stuff i've been doing
    - a bunch of stuff for the main menu which mostly works
    - keyboard accessibility
    - added ghost transformation
    - added possession

--- Assets/Scripts/SoundScripts/VolumeSettings.cs
+++ Assets/Scripts/SoundScripts/VolumeSettings.cs
@@ -9,10 +9,17 @@ public class VolumeSettings : MonoBehaviour
     // These are needed so that we can connect the slider and audio mixer to this script
 
     [SerializeField] private AudioMixer settingsMixer;
+    [SerializeField] private Slider masterSlider;
 
     [SerializeField] private Slider musicSlider;
 
     [SerializeField] private Slider sfxSlider;
+
+
+    // store the last saved music volume
+    float savedMasterVolume;
+    float savedMusicVolume;
+    float savedSFXVolume;
 
     private void Start() {
         if (PlayerPrefs.HasKey("musicVolume")) { // Loads Volume when game starts
@@ -23,8 +30,13 @@ public class VolumeSettings : MonoBehaviour
             
             SetMusicVol();
             SetSFXVol();
-
+            SetMasterVol();
         }
+
+        // save initial slider values
+        savedMasterVolume = masterSlider.value;
+        savedMusicVolume = musicSlider.value;
+        savedSFXVolume = sfxSlider.value;
     }
 
 // Connecting the Slider to the Audio Mixer, letting us change volume
@@ -44,15 +56,44 @@ public class VolumeSettings : MonoBehaviour
         PlayerPrefs.SetFloat("sfxVolume", volume); // Save the music level setting
     }
 
+    public void SetMasterVol()
+    {
+        float volume = musicSlider.value;
+        settingsMixer.SetFloat("master", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("masterVolume", volume);
+    }
+
 // Load up previous sound volume that was set
-    private void LoadVolume() {
+    public void LoadVolume() {
         
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume"); // Set initial value
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume"); // Set initial value
 
         SetMusicVol(); // Calls to actually set value
         SetSFXVol();
+        SetMasterVol();
 
     }
 
+    public bool HasSliderChanged()
+    {
+        bool masterSliderChanged = !Mathf.Approximately(masterSlider.value, savedMasterVolume);
+        bool musicSliderChanged = !Mathf.Approximately(musicSlider.value, savedMusicVolume);
+        bool sfxSliderChanged = !Mathf.Approximately(sfxSlider.value, savedSFXVolume);
+        return musicSliderChanged || sfxSliderChanged || masterSliderChanged;
+    }
+
+    public void SaveCurrentSliderValues()
+    {
+        savedMasterVolume = masterSlider.value;
+        savedMusicVolume = musicSlider.value;
+        savedSFXVolume = sfxSlider.value;
+    }
+
+    public void resetSliderValues()
+    {
+        masterSlider.value = 0.5f;
+        musicSlider.value = 0.5f;
+        sfxSlider.value = 0.5f;
+    }
 }
